314e9bf0f959430d43e21ddab40d98b7b2c334e3
Hakag3 <wolfex2016@yandex.ru>
2024-05-01 15:57:14 +0300

    alfa two

--- main.js
+++ main.js
@@ -1,6 +1,19 @@
 function setNumber(number) {
-    let resultString = document.getElementById('result');
+    let resultString = document.getElementById('display');
     let currentText = resultString.textContent;
     resultString.innerText = currentText + number
 
 }
+function clearDisplay() {
+    let resultString = document.getElementById('display');
+    resultString.innerText = ''
+}
+
+function calculate() {
+    let resultString = document.getElementById('display');
+    resultString.innerText = eval(resultString.innerText);
+
+
+
+}
+
