f4b4703b32bf86395310ae4b3f4ceadef374eb9f
Hakag3 <wolfex2016@yandex.ru>
2024-05-01 14:07:36 +0300

    alfa calculator
    alfa

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-function serNumber(number) {
+function setNumber(number) {
     let resultString = document.getElementById('result');
     let currentText = resultString.textContent;
     resultString.innerText = currentText + number
